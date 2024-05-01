6052584112772b4408b5127e1d6db2ce114977ad
hanjing <hanjing2020@foxmail.com>
2024-05-01 14:05:07 +0800

    example heaptimer

---
+++ example/ex_heaptimer.cpp
@@ -0,0 +1,17 @@
+#include <spdlog/spdlog.h>
+#include <heaptimer.h>
+#include <unistd.h>
+using namespace std::chrono_literals;
+
+
+int main() {
+    HeapTimer heap_timer;
+    heap_timer.Add(1, 10s, [&] { heap_timer.Del(1);}) ;
+    heap_timer.Add(2, 10s, [&] { heap_timer.Del(2);});
+
+    heap_timer.Add(3, 10s, [&] { heap_timer.Del(3);});
+    heap_timer.Extend(1, 10s);
+    sleep(11);
+    heap_timer.Del(1);
+    heap_timer.GetNextTick();
+}
